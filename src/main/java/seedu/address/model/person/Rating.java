c360e95fc4c8ee5b1030679a12ec4b8c8d8e45dd
Terence Goh <yangyu43@gmail.com>
2018-10-11 11:43:53 +0800

    Rating: Change validation lower end-point from 1 to 0

--- src/main/java/seedu/address/model/person/Rating.java
+++ src/main/java/seedu/address/model/person/Rating.java
@@ -11,8 +11,8 @@ public class Rating {
 
     public static final Rating DEFAULT_INITIAL_RATING = new Rating("0");
     public static final String MESSAGE_CONSTRAINTS =
-            "Ratings should only contain numbers, and it should be between 0 to 10";
-    public static final String VALIDATION_REGEX = "[0-9]|10";
+            "Ratings should only contain numbers, and it should be between 1 to 10";
+    public static final String VALIDATION_REGEX = "[1-9]|10";
     public final String value;
 
 
