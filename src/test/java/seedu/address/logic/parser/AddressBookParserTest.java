63290357a2d83f55770528ea5985055d2ac8896d
Terence Goh <yangyu43@gmail.com>
2018-10-11 20:45:24 +0800

    AddressBookParserTest: Update parseCommand_add with Person of "0" Rating

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -44,7 +44,7 @@ public class AddressBookParserTest {
 
     @Test
     public void parseCommand_add() throws Exception {
-        Person person = new PersonBuilder().build();
+        Person person = new PersonBuilder().withRating("0").build(); // Rating set to 0 to simulate newly added staff
         AddCommand command = (AddCommand) parser.parseCommand(PersonUtil.getAddCommand(person));
         assertEquals(new AddCommand(person), command);
     }
