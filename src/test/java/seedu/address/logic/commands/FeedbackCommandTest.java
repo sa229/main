a2b39594e72d817bf9277eadafde98b31969d41d
Terence Goh <yangyu43@gmail.com>
2018-10-24 20:50:01 +0800

    Fix more checkstyle tests

--- src/test/java/seedu/address/logic/commands/FeedbackCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FeedbackCommandTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
-import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
