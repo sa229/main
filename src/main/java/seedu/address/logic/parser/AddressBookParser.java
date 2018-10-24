99ca6d21a15f6ff090c8c0594b2098b53e8dda03
Terence Goh <yangyu43@gmail.com>
2018-10-24 18:39:24 +0800

    AddressBookParser: Accept FeedbackCommand to return a FeedbackCommand with null arguments

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -90,7 +90,7 @@ public class AddressBookParser {
             return new RateCommandParser().parse(arguments);
 
         case FeedbackCommand.COMMAND_WORD:
-            return null;
+            return new FeedbackCommand(null);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
