4bc5d1847093f37cd871aba389b22bde53581745
Terence Goh <yangyu43@gmail.com>
2018-11-01 02:11:40 +0800

    FeedbackTest: Add tests to check if profanities get rejected

--- src/test/java/seedu/address/model/person/FeedbackTest.java
+++ src/test/java/seedu/address/model/person/FeedbackTest.java
@@ -37,6 +37,21 @@ public class FeedbackTest {
     }
 
     @Test
+    public void hasNoProfanityFeedback() {
+        // feedback with profanity --> rejected
+        assertFalse(Feedback.hasNoProfanity("fuck")); // one profanity
+        assertFalse(Feedback.hasNoProfanity("fuck asshole")); // profanities split with space
+        assertFalse(Feedback.hasNoProfanity("zkanasaiz")); // profanities within word
+        assertFalse(Feedback.hasNoProfanity("You very kpkb, nobody ask you to stfu meh "
+            + "jibai pubor kia")); // many profanities
+
+        // clean feedback --> accepted
+        assertTrue(Feedback.hasNoProfanity("Work harder."));
+        assertTrue(Feedback.hasNoProfanity("What happened to you? You were really rocking it last month, "
+                + "but it is evident that you are sloppy these few days.")); // long feedback
+    }
+
+    @Test
     public void equals() {
         Feedback positiveFeedback = new Feedback("You are good!");
         Feedback negativeFeedback = new Feedback("You are bad!");
