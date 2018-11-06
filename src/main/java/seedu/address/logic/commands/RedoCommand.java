dd2de96ad8d8e785d5ee76565f5f0061a813b1a4
DanKhoo <danielkhoo94@hotmail.com>
2018-11-07 01:30:55 +0800

    Add event trigger when edit, feedback, privacy, rate, redo, undo commands on staffPanel

--- src/main/java/seedu/address/logic/commands/RedoCommand.java
+++ src/main/java/seedu/address/logic/commands/RedoCommand.java
@@ -3,6 +3,8 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.HideStaffPanelEvent;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
@@ -13,7 +15,8 @@ import seedu.address.model.Model;
 public class RedoCommand extends Command {
 
     public static final String COMMAND_WORD = "redo";
-    public static final String MESSAGE_SUCCESS = "Redo success!";
+    public static final String MESSAGE_SUCCESS = "Redo success!" + "\n"
+            + "Any staff panel previously selected will now be unselected";
     public static final String MESSAGE_FAILURE = "No more commands to redo!";
 
     @Override
@@ -26,6 +29,8 @@ public class RedoCommand extends Command {
 
         model.redoAddressBook();
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        EventsCenter.getInstance().post(new HideStaffPanelEvent());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
