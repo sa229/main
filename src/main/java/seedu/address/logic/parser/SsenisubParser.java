cfde5c3e155eadaef883ce0269450aa98ac2567a
Valensia Sebastiani <valensia_s@Valensias-MacBook-Air.local>
2018-11-12 02:13:55 +0800

    add tests for the different sort commands

--- src/main/java/seedu/address/logic/parser/SsenisubParser.java
+++ src/main/java/seedu/address/logic/parser/SsenisubParser.java
@@ -6,7 +6,32 @@ import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import seedu.address.logic.commands.*;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FavouriteCommand;
+import seedu.address.logic.commands.FeedbackCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindDepartmentCommand;
+import seedu.address.logic.commands.FindEmailCommand;
+import seedu.address.logic.commands.FindManagerCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.PrivacyCommand;
+import seedu.address.logic.commands.RateCommand;
+import seedu.address.logic.commands.RedoCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.SortCommand;
+import seedu.address.logic.commands.SortDeptCommand;
+import seedu.address.logic.commands.SortRatingDownCommand;
+import seedu.address.logic.commands.SortRatingUpCommand;
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.logic.commands.UnfavouriteCommand;
+
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
